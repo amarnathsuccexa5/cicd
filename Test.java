225274ed63c5efe1bf351a0bc5a4e7ebdcd2e848
byecoding <103449369+byecoding@users.noreply.github.com>
2022-10-16 10:29:29 +0530

    made changes for rebuild

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test file is working");
+        System.out.println("Test file is working build test");
     }
 }
